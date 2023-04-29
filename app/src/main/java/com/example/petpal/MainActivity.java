d5a899f7b69385cf0e5ac494e61dc0f57ffa807c
Stanis232 <st.yaruchyk@gmail.com>
2023-04-29 20:13:54 +0200

    Creacion icono y primer layout

--- app/src/main/java/com/example/petpal/MainActivity.java
+++ app/src/main/java/com/example/petpal/MainActivity.java
@@ -2,13 +2,22 @@ package com.example.petpal;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.os.AsyncTask;
 import android.os.Bundle;
+import android.widget.ProgressBar;
 
 public class MainActivity extends AppCompatActivity {
-
+    ProgressBar progressBar;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+
         setContentView(R.layout.activity_main);
+         progressBar = (ProgressBar) findViewById(R.id.determinate_linear_indicator);
+        int maxValue=progressBar.getMax(); // get maximum value of the progress bar
     }
+
+
+
+
 }
