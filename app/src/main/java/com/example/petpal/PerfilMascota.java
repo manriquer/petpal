fb105a9c5a14b2fc5d4ffd5f15db0a91db9f8855
manriruma <48059753+manriruma@users.noreply.github.com>
2023-05-10 08:15:18 +0200

    prueba maps

--- app/src/main/java/com/example/petpal/PerfilMascota.java
+++ app/src/main/java/com/example/petpal/PerfilMascota.java
@@ -1,11 +1,12 @@
 package com.example.petpal;
 
-import android.annotation.SuppressLint;
 import android.app.AlertDialog;
 import android.content.DialogInterface;
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.EditText;
 import android.widget.ImageView;
 import android.widget.TextView;
 
@@ -14,8 +15,7 @@ import androidx.appcompat.app.AppCompatActivity;
 public class PerfilMascota extends AppCompatActivity {
     ImageView logo,foto;
     TextView info;
-    Button darpaseo, añadirreco;
-
+    Button darpaseo, anyadirreco;
 
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -24,19 +24,23 @@ public class PerfilMascota extends AppCompatActivity {
         foto = findViewById(R.id.foto);
         info = findViewById(R.id.info);
         darpaseo = findViewById(R.id.darpaseo);
-        añadirreco = findViewById(R.id.añadirrecorrido);
+        anyadirreco = findViewById(R.id.recordatorio);
+//        darpaseo.setOnClickListener(new View.OnClickListener() {
+//            @Override
+//            public void onClick(View v) {
+//                mostrarConfirmacion();
+//            }
+//        });
 
-        darpaseo.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                mostrarConfirmacion();
-            }
-        });
+    }
+
+    public void paseo(View view) {
+        Intent intent = new Intent(this, MapsActivity.class);
+        startActivity(intent);
     }
     private void mostrarConfirmacion() {
         AlertDialog.Builder builder = new AlertDialog.Builder(this);
         builder.setTitle("¿Con mas mascotas?");
-
 
         builder.setPositiveButton("Sí", new DialogInterface.OnClickListener() {
             @Override
