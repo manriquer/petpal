8c0c8ea1096dc151a7764dfc1a40e30bdb8f758b
Stanis232 <st.yaruchyk@gmail.com>
2023-05-23 18:29:30 +0200

    Intento de algo

--- app/src/main/java/com/example/petpal/MainActivity.java
+++ app/src/main/java/com/example/petpal/MainActivity.java
@@ -1,8 +1,11 @@
 package com.example.petpal;
 
+import static android.content.ContentValues.TAG;
+
 import android.graphics.Bitmap;
 import android.graphics.BitmapFactory;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.MenuItem;
 import android.view.View;
 import androidx.appcompat.app.AppCompatActivity;
@@ -13,12 +16,17 @@ import androidx.recyclerview.widget.RecyclerView;
 
 import com.google.android.material.appbar.MaterialToolbar;
 import com.google.android.material.floatingactionbutton.FloatingActionButton;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
 
 import java.util.ArrayList;
 import java.util.List;
 
 public class MainActivity extends AppCompatActivity implements AddPetDialog.OnAgregarAnimalListener {
-
+    private  DatabaseReference mDatabase;
     RecyclerView recyclerViewAnimales;
     List<Pet> animales;
     FloatingActionButton fab;
@@ -29,6 +37,7 @@ public class MainActivity extends AppCompatActivity implements AddPetDialog.OnAg
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
 
         // TOP APP BAR:
         MaterialToolbar topAppBar = findViewById(R.id.topAppBar);
@@ -54,18 +63,45 @@ public class MainActivity extends AppCompatActivity implements AddPetDialog.OnAg
         fab.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
+                // Write a message to the database
+                FirebaseDatabase database = FirebaseDatabase.getInstance();
+                DatabaseReference myRef = database.getReference("strings");
+
+                myRef.setValue("Hello, World!");
+                // Read from the database
+                myRef.addValueEventListener(new ValueEventListener() {
+                    @Override
+                    public void onDataChange(DataSnapshot dataSnapshot) {
+                        // This method is called once with the initial value and again
+                        // whenever data at this location is updated.
+                        String value = dataSnapshot.getValue(String.class);
+                        Log.d(TAG, "Value is: " + value);
+                        System.out.println(value);
+                    }
+
+                    @Override
+                    public void onCancelled(DatabaseError error) {
+                        // Failed to read value
+                        Log.w(TAG, "Failed to read value.", error.toException());
+                    }
+                });
                 AddPetDialog dialogo = new AddPetDialog();
                 dialogo.show(getSupportFragmentManager(), "MiDialogoPersonalizado");
             }
         });
 
         // LISTA:
+
+
         animales = new ArrayList<>();
         Bitmap bitmap = BitmapFactory.decodeResource(getResources(), R.drawable.ic_arrow_back_24dp);
         Pet gato= new Pet("Stanis","salvaje","3kg","15/06/2003", bitmap);
         animales.add(gato);
+
+
         Pet perro = new Pet("faeba","sfinx","100kg","23/03/2000A.C", bitmap);
         animales.add(perro);
+
 
         recyclerViewAnimales = findViewById(R.id.recyclerViewAnimales);
         adaptador = new PetsAdapter(animales, this);
