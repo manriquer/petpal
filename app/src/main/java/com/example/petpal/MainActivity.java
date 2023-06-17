227c01bf7def51a7a2fe32b6790335a4e52c6407
Stanis232 <st.yaruchyk@gmail.com>
2023-06-17 20:56:17 +0200

    bOTON borrar perfil mal hecho

--- app/src/main/java/com/example/petpal/MainActivity.java
+++ app/src/main/java/com/example/petpal/MainActivity.java
@@ -295,6 +295,7 @@ public class MainActivity extends AppCompatActivity implements DialogAddPet.OnAg
                                                     intent.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP | Intent.FLAG_ACTIVITY_NEW_TASK);
                                                     startActivity(intent);
                                                     finish();
+
                                                 }
 
                                             })
